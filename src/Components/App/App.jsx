8c5952fa4b7b3b9ebf210d5b06cf2a006f5a958a
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-01-14 01:08:50 -0500

    Functionality feature to save playlist and change playlist name

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -16,13 +16,15 @@ export default class App extends React.Component {
     ],
       playlistName: "Coding Playlist",
       playlistTracks: [
-        {name: "name1", artist:"artist1", album: "album1", id: 1},
-        {name: "name2", artist:"artist2", album: "album2", id: 2},
-        {name: "name3", artist:"artist3", album: "album3", id: 3}
+        {name: "name1", artist:"artist1", album: "album1", id: 1, uri: 1231},
+        {name: "name2", artist:"artist2", album: "album2", id: 2, uri: 123123},
+        {name: "name3", artist:"artist3", album: "album3", id: 3, uri: 9847098324}
       ]
     }
     this.addTrack=this.addTrack.bind(this);
     this.removeTrack=this.removeTrack.bind(this);
+    this.updatePlaylistName=this.updatePlaylistName.bind(this);
+    this.savePlaylist= this.savePlaylist.bind(this);
   }
   // Method to add a song to the playlist state
   addTrack(track) {
@@ -48,6 +50,15 @@ export default class App extends React.Component {
     this.setState({playlistTracks : newPlTrack})
   }
 
+  updatePlaylistName(name){
+    this.setState({playlistName : name})
+  }
+
+  savePlaylist(){
+    let trackUris= []
+    let uriTrackUpdater= this.state.playlistTracks.forEach(track => trackUris.push(track.uri));
+  }
+
   render() {
     return(
         <div>
@@ -56,7 +67,12 @@ export default class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+              <Playlist playlistName={this.state.playlistName}
+               playlistTracks={this.state.playlistTracks}
+                onRemove={this.removeTrack}
+                onNameChange={this.updatePlaylistName}
+                onSave={this.savePlaylist}
+                />
             </div>
           </div>
         </div>
--- src/Components/Playlist/Playlist.jsx
+++ src/Components/Playlist/Playlist.jsx
@@ -4,12 +4,21 @@ import "./Playlist.css";
 import Tracklist from "../Tracklist/Tracklist";
 
 export default class Playlist extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleNameChange=this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e){
+    this.props.onNameChange(e.target.value);
+  }
+
   render(){
     return(
       <div className="Playlist">
-        <input defaultValue={'New Playlist'}/>
+        <input onChange={this.handleNameChange} defaultValue={'New Playlist'}/>
         <Tracklist onRemove={this.props.onRemove} isRemoval={true} tracks={this.props.playlistTracks}/>
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button onClick={this.props.onSave} className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
   }
