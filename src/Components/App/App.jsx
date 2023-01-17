532b3c60fa802beace142c5e4eac0e2087d48b8c
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-01-16 20:45:32 -0500

    new add

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -71,7 +71,7 @@ export default class App extends React.Component {
   render() {
     return(
         <div>
-          <h1>Ja<span className="highlight">mmm</span>ing<br>Search songs, Make Playlists & Upload them to Spotify</br></h1>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
             <SearchBar onSearch={this.search}/>
             <div className="App-playlist">
